9debee19dda4b54d892dfd4ef1331142e872a7b1
Vayla <manager@vayla.io>
2026-02-15 09:33:34 +0700

    feat: Configure Next.js for static export and add a GitHub Actions workflow for automated FTP deployment.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
-    remotePatterns: [],
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
   },
 };
 
 module.exports = nextConfig;
-
